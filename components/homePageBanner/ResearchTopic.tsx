647ccef216b09ba8e23266892068084dc170ba10
Sam5566 <samhuang87@gmail.com>
2023-07-15 21:57:57 +0800

    modified:   components/homePageBanner/ResearchTopic.tsx 	modified:   package-lock.json 	modified:   package.json

--- components/homePageBanner/ResearchTopic.tsx
+++ components/homePageBanner/ResearchTopic.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Controller, Scene } from 'react-scrollmagic';
 
 type Props = {};
@@ -30,24 +30,48 @@ const data = [
 ];
 
 function ResearchTopic({}: Props) {
+    const [OnHoverTopic, SetOnHoverTopic] = useState(-1)
+    const handleMouseEnter = (index:any):any => {
+        SetOnHoverTopic(index);
+    };
+    const handleMouseLeave = (index:any):any => {
+        SetOnHoverTopic(-1);
+    };
     return (
         <section id="researchTopic" className="h-[400vh] flex">
             <Controller>
-                <Scene duration={'300%'} triggerHook={0}>
+                <Scene pin duration={'300%'} triggerHook={0}>
                     <div id="title" className="w-1/2 h-screen !m-0 text-4xl center">
                         <h2>Research Topic</h2>
                     </div>
                 </Scene>
             </Controller>
-            <div id="container" className="w-1/2">
-                {data.map((item, index) => {
-                    return (
-                        <div key={index} className={`h-screen flex flex-col justify-around items-center ` + item.background}>
-                            <h2 className="text-4xl">{item.title}</h2>
-                            <p className="w-4/5 text-lg">{item.content}</p>
-                        </div>
-                    );
-                })}
+            <div id="container" className="h-[400vh] w-1/2">
+                <div className='h-screen'>
+                    image
+                </div>
+                    <div className='h-screen'>
+                    {data.map((item, index) => {
+                        return (
+                            <div className='h-1/5'>
+                                <div key={index} className={`h-full flex flex-col justify-around items-start ` + item.background} 
+                                onMouseEnter={(index) => handleMouseEnter(index)}
+                                onMouseLeave={(index) => handleMouseLeave(index)}>
+                                    <h2 className="text-4xl">{item.title}</h2>
+                                </div>
+                                {OnHoverTopic>index ? 
+                                <div className='h-1/5 inline-block w-full'>
+                                    <p className={`h-full text-lg flex flex-col justify-around items-start ` + item.background}>{item.content}</p>
+                                </div> : 
+                                <div className='h-1/5 hidden'>
+                                    <p className={`h-full text-lg flex flex-col justify-around items-start ` + item.background}>{item.content}</p>
+                                </div>}
+                                
+                            </div>
+                        );
+                    })}
+                </div>
+                
             </div>
         </section>
     );
